ef810d8da6f0ed790816d879e907b4e03351d2c8
DanJtl <daniel_junttila@live.se>
2022-01-19 11:36:10 +0000

    Add getters for player class

--- run.py
+++ run.py
@@ -6,10 +6,34 @@ class Player:
     Creates base class for player
     """
     def __init__(self, player_name, player_ap, player_health, player_luck):
-        self.player_name = player_name
-        self.player_ap = player_ap
-        self.player_health = player_health
-        self.player_luck = player_luck
+        self.name = player_name
+        self.ap = player_ap
+        self.health = player_health
+        self.luck = player_luck
 
-    def
+    """ 
+    Getters for player class
+    Learned from: https://www.geeksforgeeks.org/getter-and-setter-in-python/ 
+    """
+    def get_name(self):
+        return self._name
+
+    def get_ap(self):
+        return self._ap
+
+    def get_health(self):
+        return self._health
+    
+    def get_luck(self):
+        return self._luck
+
+
+
+
+
+
+
+
+
+
 
