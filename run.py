52615cdd0865fd01a8bf8adee7a43dcfc7d5df53
DanJtl <daniel_junttila@live.se>
2022-01-19 15:49:25 +0000

    Fix problems in run.py

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 import math
 import random
+from pprint import pprint
+
 
 class Player:
     """
@@ -11,8 +13,11 @@ class Player:
         self.health = player_health
         self.luck = player_luck
 
-    # Getters for player class - so you can, for example, check/return health 
-    # (getters and setters learned from:) https://medium.com/@pranaygore/setters-and-getters-in-python-76b5473b3c83
+    """
+    Getters for player class - so you can, for example, check/return health.
+    Getters and setters learned from:
+    https://medium.com/@pranaygore/setters-and-getters-in-python-76b5473b3c83
+    """
     def getName(self):
         return self.name
 
@@ -21,32 +26,36 @@ class Player:
 
     def getHealth(self):
         return self.health
-    
+
     def getLuck(self):
         return self.luck
 
-    # Setters for player class - so you can, for example, update the health attribute
+    # Setters for player class - so you can update the health attribute e.g.
     def setName(self, update_name):
         self.name = update_name
 
     def setAp(self, update_ap):
-        self.ap = update_ap)
+        self.ap = update_ap
 
     def setHealth(self, update_health):
         self.health = update_health
-    
+ 
     def setLuck(self, update_luck):
         self.luck = update_luck
 
+
 def player_creation():
     """
-    Create a player by letting the player answer some questions 
+    Create a player by letting the player answer some questions
     """
-    print("You are once again led by three guards towards the massive iron gate…")
+    print("You are once again led by three guards towards the iron gate…")
     print("The iron gate opens slowly, and you feel…")
 
-    # Sets the player attributes and making sure they input correct data by using a while loop
-    bonus_attribute = input("a) ...extra strong.\nb) ...young and athletic.\nc) ...lucky and pumped.\n").lower()
+    """
+    Sets the player attributes and making sure they
+    input correct data by using a while loop
+    """
+    player_stats = input("a) ...extra strong.\nb) ...young and athletic.\nc) ...lucky and pumped.\n").lower()
     while player_stats != "a" and player_stats != "b" and player_stats != "c":
         print("You need to type: a, b or c...")
         print("The iron gate opens slowly, and you feel…")
@@ -67,16 +76,19 @@ def player_creation():
         Player.health = 100
         Player.luck = 8
 
-    print("The guards turn to you, and you know it's time to pick a weapon")
+    print("The guards turn to you, and you know it's time to pick a weapon...")
     print("Which one do you choose?")
-    
-    # Add to specific player attribute depending on their choice and making sure they input correct data by using a while loop
+
+    """
+    Add to specific player attribute depending on their choice,
+    and making sure they input correct data by using a while loop
+    """
     weapon_choice = input("a) Executioner's Sword.\nb) Sword and shield.\nc) Halberd.\n").lower()
     while weapon_choice != "a" and weapon_choice != "b" and weapon_choice != "c":
         print("You need to type: a, b or c...")
         print("Which one do you choose?")
         weapon_choice = input("a) Executioner's Sword.\nb) Sword and shield.\nc) Halberd.\n").lower()
-    
+
     if weapon_choice == "a":
         Player.ap += 30
 
@@ -85,18 +97,17 @@ def player_creation():
         Player.health += 20
 
     elif weapon_choice == "c":
-        Player.ap += 20 
-        Player.luck += 2 
+        Player.ap += 20
+        Player.luck += 2
 
     print("As you enter the arena, you hear the crowd goes wild...")
-    Player.name = input("what is your name?")
-    print(Player.name.upper(), "! echoes through the arena")
+    Player.name = input("What is your name?\n")
+    print(Player.name.upper(), "! ...echoes through the arena.")
+    print("Your stats are:\n")
+    return (Player.name, Player.ap, Player.health, Player.luck)
 
+game = player_creation()
 
+player_character = Player(game[0], game[1], game[2], game[3])
 
-
-
-
-
-
-
+pprint(vars(player_character))
