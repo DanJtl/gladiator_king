47ca71a9cc646a637d50dd368621cf3eeb4e3cef
DanJtl <daniel_junttila@live.se>
2022-01-19 14:24:25 +0000

    Add while loops and if/else statements in player creation function

--- run.py
+++ run.py
@@ -38,33 +38,59 @@ class Player:
     def setLuck(self, update_luck):
         self.luck = update_luck
 
-def PlayerCreation():
+def player_creation():
     """
     Create a player by letting the player answer some questions 
     """
     print("You are once again led by three guards towards the massive iron gate…")
     print("The iron gate opens slowly, and you feel…")
 
+    # Sets the player attributes and making sure they input correct data by using a while loop
     bonus_attribute = input("a) ...extra strong.\nb) ...young and athletic.\nc) ...lucky and pumped.\n").lower()
-    while bonus_attribute != "a" and bonus_attribute != "b" and bonus_attribute != "c":
+    while player_stats != "a" and player_stats != "b" and player_stats != "c":
         print("You need to type: a, b or c...")
         print("The iron gate opens slowly, and you feel…")
-        bonus_attribute = input("a) ...extra strong.\nb) …young and athletic.\nc) …lucky and pumped.\n").lower()
+        player_stats = input("a) ...extra strong.\nb) …young and athletic.\nc) …lucky and pumped.\n").lower()
 
-    if bonus_attribute == "a":
+    if player_stats == "a":
         Player.ap = 40
         Player.health = 80
-        Player.luck = 6
+        Player.luck = 5
 
-    elif bonus_attribute == "b":
+    elif player_stats == "b":
         Player.ap = 20
         Player.health = 140
-        Player.luck = 6
+        Player.luck = 5
 
-    elif bonus_attribute == "c":
+    elif player_stats == "c":
         Player.ap = 20
         Player.health = 100
-        Player.luck = 9
+        Player.luck = 8
+
+    print("The guards turn to you, and you know it's time to pick a weapon")
+    print("Which one do you choose?")
+    
+    # Add to specific player attribute depending on their choice and making sure they input correct data by using a while loop
+    weapon_choice = input("a) Executioner's Sword.\nb) Sword and shield.\nc) Halberd.\n").lower()
+    while weapon_choice != "a" and weapon_choice != "b" and weapon_choice != "c":
+        print("You need to type: a, b or c...")
+        print("Which one do you choose?")
+        weapon_choice = input("a) Executioner's Sword.\nb) Sword and shield.\nc) Halberd.\n").lower()
+    
+    if weapon_choice == "a":
+        Player.ap += 30
+
+    elif weapon_choice == "b":
+        Player.ap += 10
+        Player.health += 20
+
+    elif weapon_choice == "c":
+        Player.ap += 20 
+        Player.luck += 2 
+
+    print("As you enter the arena, you hear the crowd goes wild...")
+    Player.name = input("what is your name?")
+    print(Player.name.upper(), "! echoes through the arena")
 
 
 
