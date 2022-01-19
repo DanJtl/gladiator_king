87f8819ef7143e02df67822377fbaaceb9ccd8c9
DanJtl <daniel_junttila@live.se>
2022-01-19 11:11:53 +0000

    Import math and random, create class Player

---
+++ run.py
@@ -0,0 +1,15 @@
+import math
+import random
+
+class Player:
+    """
+    Creates base class for player
+    """
+    def __init__(self, player_name, player_ap, player_health, player_luck):
+        self.player_name = player_name
+        self.player_ap = player_ap
+        self.player_health = player_health
+        self.player_luck = player_luck
+
+    def
+
