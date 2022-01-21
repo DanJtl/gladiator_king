bf1d5d2e3ac3a9b676dc0b6eef8c979a2aab5705
DanJtl <daniel_junttila@live.se>
2022-01-21 21:00:09 +0000

    Fix so class attributes can be accessed through a variable

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import random
 from pprint import pprint
 
 
-class Player_char:
+class Player:
     """
     Creates class for player
     """
@@ -13,7 +13,7 @@ class Player_char:
         self.luck = luck
 
 
-class Enemy_char:
+class Enemy:
     """
     Creates base class for enemies
     """
@@ -24,7 +24,7 @@ class Enemy_char:
         self.luck = luck
 
 
-class Elite(Enemy_char):
+class Elite(Enemy):
     """
     Create elite enemies by inherit from enemy class
     """
@@ -39,6 +39,7 @@ def create_player():
     It also sets the player attributes and making sure they
     input correct data by using a while loop.
     """
+    player = Player("Test", 0, 0, 0,)
     print("You are once again led by three guards towards the iron gate…")
     print("The iron gate opens slowly, and you feel…")
 
@@ -70,7 +71,7 @@ def create_player():
         player.health = 100
         player.luck = random.randint(5, 8)
 
-    print("\nThe guards turn to you...")
+    print("The guards turn to you...")
     print("\n...and you know it's time to pick a weapon...")
     print("Which one do you choose?")
 
@@ -102,14 +103,15 @@ def create_player():
     player.name = input("What is your name?\n")
     print(f"\n{player.name.upper()}! ...echoes through the arena.\n")
     print("Your stats are:")
-
-    return Player_char(player.name, player.dmg, player.health, player.luck)
+    pprint(vars(player))
+    return player
 
 
 def create_enemy():
     """
     Create an enemy and set enemy stats
     """
+    enemy = Enemy("Test", 0, 0, 0,)
     enemy_first = (
             "Angry", "Big", "Aggresive", "Furious", "Crazy",
             "Creepy", "Dangerous", "Evil", "Powerful", "Scary"
@@ -143,8 +145,7 @@ def create_enemy():
         enemy.dmg = random.randint(1, 15)
         enemy.health = random.randint(50, 120)
         enemy.luck = random.randint(1, 6)
-
-        return Enemy_char(enemy.name, enemy.dmg, enemy.health, enemy.luck)
+    return enemy
 
 
 def enemy_attack():
@@ -188,15 +189,15 @@ def player_attack():
 
     if attack == "a":
         print("You raise your wepaon...")
-        print(f"and attack the {enemy.name} with a fast attack")
+        print(f"and attack the {enemy.name} with a fast attack!")
 
     elif attack == "b":
         print("You raise your wepaon...")
-        print(f"and attack the {enemy.name} with a normal attack")
+        print(f"and attack the {enemy.name} with a normal attack!")
 
     elif attack == "c":
         print("You raise your wepaon...")
-        print(f"and attack the {enemy.name} with a charge attack")
+        print(f"and attack the {enemy.name} with a charge attack!")
 
     hit = random.randint(0, 10)
 
@@ -215,7 +216,6 @@ def battle():
     Battle function
     """
     while player.health > 0 and enemy.health > 0:
-
         player_attack()
         print(f"\n{enemy.name}'s health is now {enemy.health}.")
         if enemy.health <= 0:
@@ -231,10 +231,8 @@ def battle():
             print("...but it wasn't enough.")
 
 
-player = None
-enemy = None
-
+elite_enemy = None
 player = create_player()
-pprint(vars(player))
 enemy = create_enemy()
+
 battle()
